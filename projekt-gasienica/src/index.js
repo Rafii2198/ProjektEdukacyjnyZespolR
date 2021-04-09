33824b81cd4927c3fdd42c6a1d0e89bd6a2380b1
Rafii2198 <rafii2198@gmail.com>
2021-04-09 09:14:26 +0200

    Ilość Strzałów

--- projekt-gasienica/src/index.js
+++ projekt-gasienica/src/index.js
@@ -20,7 +20,7 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
